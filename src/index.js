8528354cb988bd63699ddb820ab779d104c722f5
vincentpchevalier <vincentpchevalier@proton.me>
2025-01-24 15:57:30 -0500

    add morgan tiny logger to app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import 'dotenv/config';
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
+
+app.use(morgan('tiny'));
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
