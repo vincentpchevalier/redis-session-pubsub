83bb8346052bea2ccfc97ee3b2fba9d4bdadc37a
vincentpchevalier <vincentpchevalier@proton.me>
2025-01-25 11:52:38 -0500

    scaffold all the subscribe and publish to redis functions

--- src/services/pubsub.js
+++ src/services/pubsub.js
@@ -3,7 +3,7 @@ import redis from 'redis';
 let pubClient;
 let subClient;
 
-const init = async () => {
+export const init = async () => {
 	pubClient = await redis
 		.createClient({
 			url: process.env.REDIS_URL,
@@ -17,4 +17,30 @@ const init = async () => {
 		})
 		.on('error', (err) => console.error('Redis Subscriber Error', err))
 		.connect();
+};
+
+const checkRedisConnection = async () => {
+	try {
+		// check if pubClient is open
+		// check if subClient is open
+	} catch (error) {
+		console.error('Unable to connect to Redis:', error);
+		throw error;
+	}
+};
+
+export const subscribe = async (userId, sessionId) => {
+	// add guard clause to check if connected
+};
+
+export const unsubscribe = async (userId, sessionId) => {
+	// add guard clause to check if connected
+};
+
+export const publish = async (sessionId, message) => {
+	// add guard clause to check if connected
+};
+
+export const disconnect = async () => {
+	console.log('Disconnecting from Redis clients.');
 };
