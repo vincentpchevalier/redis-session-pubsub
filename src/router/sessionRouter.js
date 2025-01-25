c67b163430ade64a4f17ef77e8afd900ee390a18
vincentpchevalier <vincentpchevalier@proton.me>
2025-01-25 10:06:50 -0500

    add routes for get, post, and delete by id

--- src/router/sessionRouter.js
+++ src/router/sessionRouter.js
@@ -3,7 +3,16 @@ import { Router } from 'express';
 const sessionRouter = Router();
 
 sessionRouter.post('/session', (_, res) => {
-	res.send('/POST session');
+	res.send('start session');
+});
+sessionRouter.get('/session/:id', (_, res) => {
+	res.send('join session');
+});
+sessionRouter.post('/session/:id', (_, res) => {
+	res.send('message');
+});
+sessionRouter.delete('/session/:id', (_, res) => {
+	res.send('close session');
 });
 
 export default sessionRouter;
