71609a682e5defe3e10cc539687e7e7381dabff3
vincentpchevalier <vincentpchevalier@proton.me>
2025-01-25 11:08:45 -0500

    build initialization function for redis client

---
+++ src/services/pubsub.js
@@ -0,0 +1,20 @@
+import redis from 'redis';
+
+let pubClient;
+let subClient;
+
+const init = async () => {
+	pubClient = await redis
+		.createClient({
+			url: process.env.REDIS_URL,
+		})
+		.on('error', (err) => console.error('Redis Publisher Error', err))
+		.connect();
+
+	subClient = await redis
+		.createClient({
+			url: process.env.REDIS_URL,
+		})
+		.on('error', (err) => console.error('Redis Subscriber Error', err))
+		.connect();
+};
