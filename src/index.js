fc5f6bd38a1c681f8906981affe9e90f8e2a1e44
vincentpchevalier <vincentpchevalier@proton.me>
2025-01-25 12:13:57 -0500

    import redisPubSub into app

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
 
+import redisPubSub from './services/pubsub.js';
 import sessionRouter from './router/sessionRouter.js';
 
 const app = express();
 
 app.use(morgan('tiny'));
 app.use(express.json());
+
+redisPubSub();
 
 app.get('/', (_req, res) => res.send('Server is running 👋'));
 
