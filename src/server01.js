996efb31d6e09f56db999e6bb0c0e0c8b0b6a237
vincentpchevalier <vincentpchevalier@proton.me>
2025-01-26 10:28:18 -0500

    create separate server files to run app in dev

---
+++ src/server01.js
@@ -0,0 +1,6 @@
+import app from './index.js';
+
+const port = 3000;
+app.listen(port, () => {
+	console.log(`App listening on port:${port}`);
+});
---
+++ src/server02.js
@@ -0,0 +1,6 @@
+import app from './index.js';
+
+const port = 4000;
+app.listen(port, () => {
+	console.log(`App listening on port:${port}`);
+});
