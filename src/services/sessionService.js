6d0133139eb6a28b13b2c6f87aedfbb358a92ceb
vincentpchevalier <vincentpchevalier@proton.me>
2025-01-25 20:10:04 -0500

    set up startSession and sendMessage functions

--- src/services/sessionService.js
+++ src/services/sessionService.js
@@ -1,17 +1,46 @@
-import { subscribe } from './pubsub';
+import crypto from 'crypto';
+
+import { publish, subscribe } from './pubsub.js';
+
+// generate token
+// add token to list (new Set) of current tokens (save as list in redis cache)
+// clear tokens after expiry or when server closes
+// prefix token with "sid_"  for session id
+
+const sessions = new Set();
+
+const generateSessionId = () => {
+	// const random = Math.floor(1000 + Math.random() * 9000);
+	const random = crypto.randomUUID();
+	console.log(random);
+	const sessionId = `sid_${random}`;
+	sessions.add(sessionId);
+
+	const token = random.slice(-4);
+	console.log(sessions);
+	console.log(token);
+
+	return { sessionId, token };
+};
 
 export const startSession = async (userId) => {
 	// generate token
+	const { sessionId, token } = generateSessionId();
 	// call subscribe using token as session id
+	await subscribe(userId, token);
+	// return userId, sessionId, code (token) in response
+	return { user: userId, sessionId, token };
 };
 
-export const sendMessage = async (sessionId, message) => {
-	// get session code/id and message from request
+export const sendMessage = async (token, message) => {
 	// call publish to session
+	await publish(token, message);
+
+	return message;
 };
 
-export const joinSession = async (sessionId) => {
-	// call subscribe using session id
+export const joinSession = async (token) => {
+	// call subscribe using token
 };
 
 export const leaveSession = async (sessionId) => {
