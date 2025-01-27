5966ded6f44d78659fe9f91e467125a7e38f1680
vincentpchevalier <vincentpchevalier@proton.me>
2025-01-26 19:42:24 -0500

    set up error handling in controllers

--- src/controllers/sessionController.js
+++ src/controllers/sessionController.js
@@ -4,11 +4,16 @@ export const createSession = async (req, res) => {
 	try {
 		const { userId } = req.body;
 
+		if (!userId) {
+			res.status(400).json({ error: 'userId not provided' });
+			return;
+		}
+
 		const code = await sessionService.startSession(userId);
 
 		res.status(201).json({ status: 'success', data: { code } });
 	} catch (err) {
-		console.error(err);
+		res.status(500).json({ error: error.message });
 	}
 };
 
@@ -16,11 +21,16 @@ export const joinSession = async (req, res) => {
 	try {
 		const { userId, code } = req.body;
 
+		if (!userId || !code) {
+			res.status(400).json({ error: 'Missing userId or code.' });
+			return;
+		}
+
 		await sessionService.joinSession(userId, code);
 
 		res.status(200).json({ status: 'joined' });
 	} catch (error) {
-		console.error(err);
+		res.status(500).json({ error: error.message });
 	}
 };
 
@@ -29,24 +39,28 @@ export const message = async (req, res) => {
 		const { code, message } = req.body;
 
 		if (!code || !message) {
-			throw new Error('Missing code or message.');
+			res.status(400).json({ error: 'Missing userId or code.' });
+			return;
 		}
 
 		await sessionService.sendMessage(code, message);
 
 		res.status(200).json({ status: 'sent' });
 	} catch (err) {
-		console.error(err);
+		res.status(500).json({ error: error.message });
 	}
 };
 
 export const leaveSession = async (req, res) => {
 	try {
 		const { userId, code } = req.body;
-		console.log(userId, code);
+		if (!userId || !code) {
+			res.status(400).json({ error: 'Missing userId or code.' });
+			return;
+		}
 		await sessionService.leaveSession(userId, code);
 		res.status(200).json({ status: `user ${userId} left session ${code}` });
 	} catch (error) {
-		console.error(error);
+		res.status(500).json({ error: error.message });
 	}
 };
