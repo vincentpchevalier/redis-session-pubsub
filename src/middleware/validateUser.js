7e845f12610941784b21bb12fa05bd9bae316b03
Vincent Pierre Chevalier <vincentpchevalier@proton.me>
2025-02-05 21:37:51 -0500

    build validateUser middleware (#9)

---
+++ src/middleware/validateUser.js
@@ -0,0 +1,27 @@
+import * as cache from '../services/cache.js';
+
+export const validateUser = async (req, res, next) => {
+	try {
+		const { sessionKey, userKey } = req.keys || {};
+
+		if (!sessionKey || !userKey) {
+			return res.status(400).json({ error: 'Missing sessionKey or userKey.' });
+		}
+
+		const sessionCode = sessionKey?.split(process.env.SESSION_KEY)[1];
+		const username = userKey?.split(process.env.USER_KEY)[1];
+
+		const isMember = await cache.cacheClient.sisMember(sessionKey, userKey);
+
+		if (!isMember) {
+			return res.status(400).json({
+				error: `${username} is not a member of session ${sessionCode}.`,
+			});
+		}
+
+		next();
+	} catch (error) {
+		console.error('User validation error:', error);
+		next(error);
+	}
+};
