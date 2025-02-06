5bd46c2dd14be9647837487744d6e0ec8555b11f
Vincent Pierre Chevalier <vincentpchevalier@proton.me>
2025-02-05 21:21:48 -0500

    create validateSession middleware (#8)

---
+++ src/middleware/validateSession.js
@@ -0,0 +1,29 @@
+import * as cache from '../services/cache.js';
+
+export const validateSession = async (req, res, next) => {
+	try {
+		const { code, userId } = req.body;
+
+		if (!code || !userId) {
+			return res.status(400).json({ error: 'Missing userId or code.' });
+		}
+
+		const sessionKey = process.env.SESSION_KEY + code;
+		const userKey = process.env.USER_KEY + userId;
+
+		const exists = await cache.exists(sessionKey);
+		if (!exists) {
+			return res.status(400).json({ error: 'Invalid session code.' });
+		}
+
+		req.keys = {
+			sessionKey,
+			userKey,
+		};
+
+		next();
+	} catch (error) {
+		console.error('Session validation error:', error);
+		next(error);
+	}
+};
