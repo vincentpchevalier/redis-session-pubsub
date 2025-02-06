4202fc6ef6f81f9175bfd26e847a7ad8fd410eff
Vincent Pierre Chevalier <vincentpchevalier@proton.me>
2025-02-05 20:41:09 -0500

    set up redis cache functionality to manage session and user ids (#6)
    * build redis cache and initialize it when app starts
    
    * build addToSet to set session key with current user
    
    * implement while loop to ensure no duplicate code errors
    
    * add env variables for cache key name and session exp in seconds
    
    * check session and return users or null if session doesn't exist
    
    * verify session exists and that the user isn't already a part of it
    
    * refine addUsers logic to not add more than two users to the session
    
    * update addUsers name
    
    * throw new Error when session invalid or the user is already in session
    
    * refine console error to include error message
    
    * add USER_KEY and change CACHE_KEY to SESSION_KEY
    
    * change CACHE_KEY to SESSION_KEY
    
    * refactor addUser into two functions
    
    * update session service functions to use new cache functions
    
    * refactor getUser to checkUser function
    
    * require userId in the request body
    
    * built removeUser on leaveSession
    
    * reorder body request object params

---
+++ src/services/cache.js
@@ -0,0 +1,104 @@
+import redis from 'redis';
+
+let cacheClient;
+
+export const init = async () => {
+	cacheClient = await redis
+		.createClient({
+			url: process.env.REDIS_URL,
+		})
+		.on('error', (error) => {
+			console.error('Redis Cache Client Error', error);
+		})
+		.connect();
+	console.log('Redis cache set up.');
+};
+
+export const createSession = async (sessionCode, userId) => {
+	const sessionKey = process.env.SESSION_KEY + sessionCode;
+	const userKey = process.env.USER_KEY + userId;
+
+	try {
+		await cacheClient.sAdd(sessionKey, userKey);
+		await cacheClient.expire(sessionKey, process.env.SESSION_EXPIRATION);
+
+		return true;
+	} catch (error) {
+		console.error(`Unable to create session: ${error.message}`);
+		throw error;
+	}
+};
+
+// set user members to cache based on sessionId
+export const addUser = async (sessionCode, userId) => {
+	const sessionKey = process.env.SESSION_KEY + sessionCode;
+	const userKey = process.env.USER_KEY + userId;
+
+	try {
+		const exists = await cacheClient.exists(sessionKey);
+
+		if (!exists) {
+			throw new Error(`Invalid session ID.`);
+		}
+
+		const userCount = await cacheClient.sCard(sessionKey);
+
+		if (userCount >= 2)
+			throw new Error(`Session ${sessionId} is already full with 2 members.`);
+
+		await cacheClient.sAdd(sessionKey, userKey);
+		await cacheClient.expire(sessionKey, process.env.SESSION_EXPIRATION);
+
+		console.log(
+			`User ${userId} added to ${sessionKey}. Expiration in ${process.env.SESSION_EXPIRATION} seconds.`
+		);
+
+		return true;
+	} catch (error) {
+		console.error(`Unable to add user ${userId}: ${error.message}`);
+		throw error;
+	}
+};
+
+// get members from cache based on sessionId
+export const checkUser = async (sessionCode, userId) => {
+	console.log('checking user');
+	// get session members from redis (if it exists)
+	const sessionKey = `${process.env.SESSION_KEY}${sessionCode}`;
+	const userKey = `${process.env.USER_KEY}${userId}`;
+	try {
+		const exists = await cacheClient.exists(sessionKey);
+		if (!exists) {
+			throw new Error(`Invalid session ID.`);
+		}
+
+		return await cacheClient.sIsMember(sessionKey, userKey);
+	} catch (error) {
+		console.error(`Unable to find user ${userId}: ${error.message}`);
+		throw error;
+	}
+};
+
+export const removeUser = async (sessionCode, userId) => {
+	// get session members from redis (if it exists)
+	const sessionKey = `${process.env.SESSION_KEY}${sessionCode}`;
+	const userKey = `${process.env.USER_KEY}${userId}`;
+	try {
+		const exists = await cacheClient.exists(sessionKey);
+		if (!exists) {
+			throw new Error(`Invalid session ID.`);
+		}
+
+		const isUser = await cacheClient.sIsMember(sessionKey, userKey);
+
+		if (isUser) {
+			await cacheClient.sRem(sessionKey, userKey);
+			return true;
+		} else {
+			throw new Error(`Invalid user ID.`);
+		}
+	} catch (error) {
+		console.error(`Unable to find user ${userId}: ${error.message}`);
+		throw error;
+	}
+};
--- src/services/sessionService.js
+++ src/services/sessionService.js
@@ -1,4 +1,5 @@
 import { publish, subscribe, unsubscribe } from './pubsub.js';
+import * as cache from './cache.js';
 
 const sessions = new Set();
 
@@ -9,37 +10,60 @@ const generateCode = () => {
 
 export const startSession = async (userId) => {
 	try {
+		let isCached;
 		const code = generateCode();
+
+		while (!isCached) {
+			isCached = await cache.createSession(code, userId);
+		}
 
 		await subscribe(userId, code);
 
 		// code sent to user in response
 		return code;
 	} catch (error) {
-		console.error(`Unable to start session for user ${userId}.`);
+		console.error(
+			`Unable to start session for user ${userId} due to: ${error.message}.`
+		);
+		throw error;
 	}
 };
 
 export const joinSession = async (userId, code) => {
 	try {
+		await cache.addUser(code, userId);
+
 		await subscribe(userId, code);
 	} catch (error) {
-		console.error(`User ${userId} unable to join session ${code}.`);
+		console.error(
+			`User ${userId} unable to join session ${code} due to: ${error.message}.`
+		);
+		throw error;
 	}
 };
 
-export const sendMessage = async (code, message) => {
+export const sendMessage = async (userId, code, message) => {
 	try {
+		const isUser = await cache.checkUser(code, userId);
+
+		if (!isUser) {
+			throw new Error('User must be in session to send message.');
+		}
+
 		await publish(code, message);
 	} catch (error) {
 		console.error(`Unable to send message.`);
+		throw error;
 	}
 };
 
 export const leaveSession = async (userId, code) => {
 	try {
+		await cache.removeUser(code, userId);
+
 		await unsubscribe(code);
 	} catch (error) {
 		console.error(`User ${userId} could not leave session ${code}.`);
+		throw error;
 	}
 };
