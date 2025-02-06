116c81ca0a077d5a4167eeade5920f4eff9c906a
vincentpchevalier <vincentpchevalier@proton.me>
2025-02-05 21:54:16 -0500

    add middleware to session routes

--- src/router/sessionRouter.js
+++ src/router/sessionRouter.js
@@ -5,12 +5,14 @@ import {
 	leaveSession,
 	message,
 } from '../controllers/sessionController.js';
+import { validateSession } from '../middleware/validateSession.js';
+import { validateUser } from '../middleware/validateUser.js';
 
 const sessionRouter = Router();
 
 sessionRouter.post('/', createSession);
-sessionRouter.get('/join', joinSession);
-sessionRouter.post('/message', message);
-sessionRouter.put('/leave', leaveSession);
+sessionRouter.get('/join', validateSession, joinSession);
+sessionRouter.post('/message', validateSession, validateUser, message);
+sessionRouter.put('/leave', validateSession, validateUser, leaveSession);
 
 export default sessionRouter;
