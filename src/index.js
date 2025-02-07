139553f61b84fa269cbea8f37efadc33e5db1993
vincentpchevalier <vincentpchevalier@proton.me>
2025-02-07 12:00:08 -0500

    import errorHandler into app index

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import * as cache from './services/cache.js';
 import redisPubSub from './services/pubsub.js';
 import sessionRouter from './router/sessionRouter.js';
+import { errorHandler } from './utils/errors.js';
 
 const app = express();
 
@@ -17,6 +18,8 @@ redisPubSub();
 app.get('/', (_req, res) => res.send('Server is running 👋'));
 
 app.use('/session', sessionRouter);
+
+app.use(errorHandler);
 
 // break into two server files for development
 // const port = process.env.PORT || 3000;
