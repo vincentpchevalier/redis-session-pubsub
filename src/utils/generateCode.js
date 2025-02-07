c2be2028ba3168f352c58c75a0f76f6b29d33eed
vincentpchevalier <vincentpchevalier@proton.me>
2025-02-07 17:05:07 -0500

    refactor generateCode to check cache for existing session name

--- src/utils/generateCode.js
+++ src/utils/generateCode.js
@@ -1,4 +1,13 @@
-export const generateCode = () => {
-	const code = Math.floor(1000 + Math.random() * 9000).toString();
+import { cacheClient } from '../services/cache.js';
+
+export const generateCode = async () => {
+	let code;
+	let exists;
+
+	do {
+		code = Math.floor(1000 + Math.random() * 9000).toString();
+		exists = await cacheClient.exists(`${process.env.SESSION_KEY}${code}`);
+	} while (exists);
+
 	return code;
 };
