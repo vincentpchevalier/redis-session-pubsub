7660892ad51d221e4df44547dde9825ba52ebd83
vincentpchevalier <vincentpchevalier@proton.me>
2025-02-07 11:46:21 -0500

    create custom Error classes with errorHandler function

---
+++ src/utils/errors.js
@@ -0,0 +1,33 @@
+export class APIError extends Error {
+	status = 500;
+
+	constructor(message) {
+		super(message);
+
+		Error.captureStackTrace(this, this.constructor);
+	}
+}
+
+export class BadRequestError extends APIError {
+	status = 400;
+}
+
+export class UnauthenticatedError extends APIError {
+	status = 401;
+}
+
+export class ForbiddenError extends APIError {
+	status = 403;
+}
+
+export class NotFoundError extends APIError {
+	status = 404;
+}
+
+export const errorHandler = (error, _req, res, _next) => {
+	if (error instanceof APIError) {
+		res.status(error.status).json({ error: error.message });
+		return;
+	}
+	res.status(500).json({ error: 'Something went wrong.' });
+};
